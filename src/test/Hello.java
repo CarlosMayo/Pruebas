3ff03ac973c832bd8e8551c1178820a2b1df055e
CarlosMayo <stipey_up2000@msn.com>
2013-06-28 13:56:04 +0200

    Update Hello.java

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -8,6 +8,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.print("hello");
         System.out.print("esto es la rama1");
+        System.out.print("lo añado en git");
     }
+    
 
 }
